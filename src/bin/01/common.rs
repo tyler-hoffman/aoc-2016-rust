feb80cb58756a6b6d77bc3fc10c2f62b9bf1b6f7
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2022-11-08 20:10:51 -0500

    Format

--- src/bin/01/common.rs
+++ src/bin/01/common.rs
@@ -1,23 +1,22 @@
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum Direction {
-  Right(i32),
-  Left(i32),
+    Right(i32),
+    Left(i32),
 }
 
 pub fn parse(input: &str) -> Result<Vec<Direction>, &'static str> {
     input
         .split(", ")
         .map(|x| {
-          let start = &x[..1];
-          let end = &x[1..];
-          let dist = end.parse::<i32>().unwrap();
+            let start = &x[..1];
+            let end = &x[1..];
+            let dist = end.parse::<i32>().unwrap();
 
-          match start {
-              "L" => Ok(Direction::Left(dist)),
-              "R" => Ok(Direction::Right(dist)),
-              _ => return Err("Invalid char found"),
-          }
+            match start {
+                "L" => Ok(Direction::Left(dist)),
+                "R" => Ok(Direction::Right(dist)),
+                _ => return Err("Invalid char found"),
+            }
         })
         .collect()
 }
