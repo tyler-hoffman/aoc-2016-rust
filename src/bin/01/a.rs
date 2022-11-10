503488fb8418e8e1a70d5a877f6f92b2304f9580
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2022-11-09 21:01:03 -0500

    Solve 01.a

--- src/bin/01/a.rs
+++ src/bin/01/a.rs
@@ -2,10 +2,58 @@ mod common;
 
 use std::error::Error;
 use std::fs;
+use common::Move;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let input = fs::read_to_string("src/bin/01/input.txt")?;
-    let directions = common::parse(&input);
-
+    let output = solve_for_input()?;
+    print!("solution: {}\n", output);
     Ok(())
+}
+
+fn solve_for_input() -> Result<i32, Box<dyn Error>> {
+    let input = fs::read_to_string("src/bin/01/input.txt")?;
+    let directions = common::parse(&input)?;
+
+    Ok(solve(directions))
+}
+
+fn solve(directions: Vec<Move>) -> i32 {
+    common::moves_dist(directions)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use common;
+
+    #[test]
+    fn case_1() {
+        let directions = common::parse("R2, L3").expect("wat");
+        let output = solve(directions);
+
+        assert_eq!(output, 5);
+    }
+
+    #[test]
+    fn case_2() {
+        let directions = common::parse("R2, R2, R2").expect("wat");
+        let output = solve(directions);
+
+        assert_eq!(output, 2);
+    }
+
+    #[test]
+    fn case_3() {
+        let directions = common::parse("R5, L5, R5, R3").expect("wat");
+        let output = solve(directions);
+
+        assert_eq!(output, 12);
+    }
+
+    #[test]
+    fn case_my_input() {
+        let output = solve_for_input().expect("wat");
+        assert_eq!(output, 226);
+    }
 }
--- src/bin/01/common.rs
+++ src/bin/01/common.rs
@@ -1,10 +1,17 @@
 #[derive(Debug, PartialEq)]
-pub enum Direction {
+pub enum Move {
     Right(i32),
     Left(i32),
 }
 
-pub fn parse(input: &str) -> Result<Vec<Direction>, &'static str> {
+enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
+
+pub fn parse(input: &str) -> Result<Vec<Move>, &'static str> {
     input
         .split(", ")
         .map(|x| {
@@ -13,12 +20,40 @@ pub fn parse(input: &str) -> Result<Vec<Direction>, &'static str> {
             let dist = end.parse::<i32>().unwrap();
 
             match start {
-                "L" => Ok(Direction::Left(dist)),
-                "R" => Ok(Direction::Right(dist)),
+                "L" => Ok(Move::Left(dist)),
+                "R" => Ok(Move::Right(dist)),
                 _ => return Err("Invalid char found"),
             }
         })
         .collect()
+}
+
+pub fn moves_dist(moves: Vec<Move>) -> i32 {
+    let mut x = 0;
+    let mut y = 0;
+    let mut direction_index = 0;
+    let directions = [Direction::North, Direction::East, Direction::South, Direction::West];
+
+    for m in moves {
+        let dist = match m {
+            Move::Right(x) => x,
+            Move::Left(x) => x,
+        };
+
+        direction_index = match m {
+            Move::Right(_) => (direction_index + 1) % 4,
+            Move::Left(_) => (direction_index + 3) % 4,
+        };
+
+        match directions[direction_index] {
+            Direction::North => y += dist,
+            Direction::South => y -= dist,
+            Direction::East => x += dist,
+            Direction::West => x -= dist,
+        };
+    };
+
+    x.abs() + y.abs()
 }
 
 #[cfg(test)]
@@ -29,7 +64,7 @@ mod tests {
     fn parsing_works() {
         let input = "L4, R-22";
         let output = parse(input).expect("wat");
-        let expected = vec![Direction::Left(4), Direction::Right(-22)];
+        let expected = vec![Move::Left(4), Move::Right(-22)];
 
         assert_eq!(expected, output);
     }
